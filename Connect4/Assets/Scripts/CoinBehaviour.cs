38c8d5733c00e28770cbed851cf98bf5be476d56
Hercules Dias Campos <hercules.amphibolite@gmail.com>
2020-02-15 02:04:20 -0500

    Created test scene, did some modification to the player, coins, and board. Tomorrow or later I'll implement the grid system and turns

---
+++ Connect4/Assets/Scripts/CoinBehaviour.cs
@@ -0,0 +1,51 @@
+﻿/***************************************************************
+ * Coin Behaviour Script
+ * By Hercules (HErC) Dias Campos (ID 101091070)
+ * Created on February 19, 2020
+ * Last Modified on February 20, 2020
+ * 
+ * Behaviour implemented to control Coin Physics
+ * Also tracks who the parent was (needed for win/loss condition
+ *     detection)
+ * Worth noting that this script requires (as of now) no Update()
+ * 
+ ***************************************************************/
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class CoinBehaviour : MonoBehaviour
+{
+    [SerializeField] private uint m_iCoinParent;
+
+    public Rigidbody m_rb;
+
+    void Awake() {
+
+        m_rb = this.gameObject.GetComponent<Rigidbody>();
+
+        if (m_rb){
+
+            m_rb.constraints = RigidbodyConstraints.FreezeAll;
+        }
+        else {
+
+            Debug.Log("Where's my Rigidbody???");
+        }
+    }
+
+    public void SetPlayerOwner(uint playerID) {
+        m_iCoinParent = playerID;
+    }
+
+    public void OnDetach() {
+
+        this.gameObject.transform.parent = null;
+        m_rb.constraints = RigidbodyConstraints.None;
+        m_rb.constraints = RigidbodyConstraints.FreezeRotation  | 
+                           RigidbodyConstraints.FreezePositionZ |
+                           RigidbodyConstraints.FreezePositionX;
+    }
+}
---
+++ Connect4/Assets/Scripts/PlayerBehaviour.cs
@@ -0,0 +1,85 @@
+﻿/***************************************************************
+ * Player Behaviour Script
+ * By Hercules (HErC) Dias Campos (101091070)
+ * Created on February 15, 2020
+ * Last Modified on February 15, 2020
+ * 
+ * Class implemented basically to control player input
+ * As it is, it's not suitable for the proposed gameplay
+ * What it (currently) does:
+ *      - Press Right arrow to move player right
+ *      - Press Left arrow to move player left
+ *      - If the player has no coin, press spacebar
+ *          to create one
+ *      - If the player has a coin, press spacebar
+ *          to release it
+ * 
+ **************************************************************/
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerBehaviour : MonoBehaviour
+{
+    [SerializeField] private GameObject m_coinTemplate;
+    private GameObject m_coin;
+    [SerializeField] private uint m_iPlayerNumber;
+    [SerializeField] private float m_fClampMin;
+    [SerializeField] private float m_fClampMax;
+
+    private bool m_bIsTurn;
+
+    void Awake() { m_bIsTurn = true; }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        CreateCoin();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (m_bIsTurn) {
+
+            if (Input.GetKeyDown(KeyCode.Space)) {
+                
+                if (m_coin) {
+
+                    ReleaseCoin();
+                }
+                else {
+                    //This will probably be refactored into a message that will be received from the server
+                    CreateCoin();
+                }
+            }
+            if (Input.GetKeyDown(KeyCode.RightArrow) && this.gameObject.transform.position.x < m_fClampMax) {
+
+                this.gameObject.transform.Translate(Vector3.right);
+            }
+            if (Input.GetKeyDown(KeyCode.LeftArrow) && this.gameObject.transform.position.x > m_fClampMin) {
+
+                this.gameObject.transform.Translate(Vector3.right * -1);
+            }
+        }
+    }
+
+    public void SetTurn(bool turn) {
+
+        m_bIsTurn = turn;
+    }
+
+    void ReleaseCoin() {
+
+        m_coin.GetComponent<CoinBehaviour>().OnDetach();
+        m_coin = null;
+        //Send Message to server, informing that the player made a move
+    }
+
+    void CreateCoin() {
+
+        m_coin = GameObject.Instantiate(m_coinTemplate, this.gameObject.transform.position, this.gameObject.transform.rotation, this.gameObject.transform);
+        m_coin.GetComponent<CoinBehaviour>().SetParentOwner(m_iPlayerNumber);
+    }
+}
